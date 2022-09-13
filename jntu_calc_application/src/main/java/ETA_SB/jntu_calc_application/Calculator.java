3a4d17951a94dceb0afb1723f0098d18a8bd2d6f
hp <hp@DESKTOP-PNOOTSM>
2022-09-13 15:09:07 +0530

    2nd commit

--- jntu_calc_application/src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ jntu_calc_application/src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -16,6 +16,9 @@ public class Calculator {
 
 	public double doDiv(double num1, double num2) {
 		return num1 / num2;
+	
 	}
-
+	public void modDiv(double num1, double num2) {
+		return num1 % num2;
+	}
 }
